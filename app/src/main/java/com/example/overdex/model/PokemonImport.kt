8d55c7f73792c08c3d273895f7a4f35da0061212
Sean <sean@example.com>
2026-06-23 13:38:47 -0500

    Add height and weight to Pokemon import model

--- app/src/main/java/com/example/overdex/model/PokemonImport.kt
+++ app/src/main/java/com/example/overdex/model/PokemonImport.kt
@@ -8,5 +8,7 @@ data class PokemonImport(
     val num: String,
     val name: String,
     val img: String,
-    val type: List<String>
+    val type: List<String>,
+    val height: String? = null,
+    val weight: String? = null
 )
